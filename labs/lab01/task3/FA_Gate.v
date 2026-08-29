0b62a3e7b4133dbabfe86097299e54f5a0dcc87e
Saumya Rawal <saumyarawal01@gmail.com>
2026-08-29 18:58:24 +0000

    lab01 task3

--- labs/lab01/task3/FA_Gate.v
+++ labs/lab01/task3/FA_Gate.v
@@ -28,8 +28,8 @@ module FA_Gate(
 
   xor #(2,3)(ps,  a,   b);
   and #(2,3)(pc1, a,   b);
-  xor #(2,3)(sum, cin, ps);
   and #(2,3)(pc2, cin, ps);
+  xor #(2,3)(sum, cin, ps);
   or  #(2,3)(cout, pc1, pc2);
 
 endmodule
